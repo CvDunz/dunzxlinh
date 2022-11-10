a02fcdfe984f9b3c57e4f71eee0ac0ccdcf0dbc1
CvDunz <103753440+CvDunz@users.noreply.github.com>
2022-11-11 00:28:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ document.addEventListener('DOMContentLoaded', function(){
       } olock();
       var timer = setInterval(function(){olock()}, 1000);
       document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
-      document.querySelector("audio").play();
       document.querySelector("audio").volume = 0.2;
       setInterval(function(){document.querySelector("footer").innerHTML = Footer_Text[Math.floor(Math.random()*Footer_Text.length)]}, 10000);
       document.getElementsByTagName("body")[0].insertAdjacentHTML(
